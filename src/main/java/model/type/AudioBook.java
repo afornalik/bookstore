216a1334fbe700256fabe5060404c69656d4bad6
Andrzej <afornalik2gmail.com>
2019-03-30 12:32:42 +0100

    add field duration in AudioBook class

--- src/main/java/model/type/AudioBook.java
+++ src/main/java/model/type/AudioBook.java
@@ -6,7 +6,15 @@ import java.math.BigDecimal;
 
 public class AudioBook extends Book {
 
-    public AudioBook(long id, String name, BigDecimal priceString, String author, int pageCounter) {
+    private int duration;
+
+    public AudioBook(long id, String name, BigDecimal priceString, String author, int pageCounter,int duration) {
+
         super(id, name, priceString, author, pageCounter);
+        this.duration = duration;
+    }
+
+    public int getDuration() {
+        return duration;
     }
 }
